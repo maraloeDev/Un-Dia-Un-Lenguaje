e971864c8a3e2c778f447884ded2ad2cad1d13a9
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2023-09-18 19:07:06 +0200

    01- C#

---
+++ 01- C#/10- Colecciones/Colecciones.cs
@@ -0,0 +1,68 @@
+﻿namespace _10__Colecciones
+{
+    internal class Colecciones
+    {
+        static void Main(string[] args)
+        {
+            //List<int> numeros;
+            //int elem = 0;
+            //try
+            //{
+            //    Console.WriteLine("Cuantos elementos quieres introducir");
+
+            //    elem = int.Parse(Console.ReadLine());
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("El numero de elementos a introducir no son correctos");
+            //    Main(null);
+            //}
+
+            //numeros = new List<int>(elem);
+
+            //try
+            //{
+
+            //    for (int i = 0; i < elem; i++)
+            //    {
+            //        numeros.Add(int.Parse(Console.ReadLine()));
+            //    }
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("El numero no es correcto");
+
+            //}
+
+            //Console.WriteLine("Elementos introducidos: ");
+
+            //foreach (var numerosIntroducidos in numeros)
+            //{
+            //    Console.WriteLine(numerosIntroducidos);
+
+
+            //}
+
+            LinkedList<int> numeros = new LinkedList<int>();
+
+            foreach (int numero in new int[] { 10, 8, 6, 4, 2, 0 })
+            {
+
+                numeros.AddFirst(numero);
+
+            }
+
+            foreach (int numero in numeros)
+            {
+
+                Console.WriteLine(numero);
+
+
+            }
+
+        }
+    }
+
+}
